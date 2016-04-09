528afeb23c6b9582f6a684ce40d6f947a6d824b0
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-04-09 11:21:52 +0530

    Implemented user classes

--- app/User.php
+++ app/User.php
@@ -32,4 +32,87 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
+
+    // user has many posts
+  	public function posts()
+  	{
+  		return $this->hasMany('App\Posts','author_id');
+  	}
+
+  	// user has many comments
+  	public function comments()
+  	{
+  		return $this->hasMany('App\Comments','from_user');
+  	}
+
+  	public function can_post()
+  	{
+  		$user_class = $this->user_class;
+  		if($user_class == 'author' || $user_class == 'moderator' || $user_class == 'editor' || $user_class == 'admin' )
+  		{
+  			return true;
+  		}
+  		return false;
+  	}
+
+  	public function can_edit()
+  	{
+  		$user_class = $this->user_class;
+  		if($user_class == 'contributor' || $user_class == 'author' || $user_class == 'moderator' || $user_class == 'editor' || $user_class == 'admin' )
+  		{
+  			return true;
+  		}
+  		return false;
+  	}
+
+  	public function is_admin()
+  	{
+  		$user_class = $this->user_class;
+  		if($user_class == 'admin')
+  		{
+  			return true;
+  		}
+  		return false;
+  	}
+
+  	public function is_editor()
+  	{
+  		$user_class = $this->user_class;
+  		if($user_class == 'editor')
+  		{
+  			return true;
+  		}
+  		return false;
+  	}
+
+  	public function is_moderator()
+  	{
+  		$user_class = $this->user_class;
+  		if($user_class == 'moderator')
+  		{
+  			return true;
+  		}
+  		return false;
+  	}
+
+  	public function is_author()
+  	{
+  		$user_class = $this->user_class;
+  		if($user_class == 'author')
+  		{
+  			return true;
+  		}
+  		return false;
+  	}
+
+  	public function is_contributor()
+  	{
+  		$user_class = $this->user_class;
+  		if($user_class == 'contributor')
+  		{
+  			return true;
+  		}
+  		return false;
+  	}
+
 }
